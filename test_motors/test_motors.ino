0b8382d2d77dd0789e53dfdf4d0310ba62891057
Ilya9291 <74893784+Ilya9291@users.noreply.github.com>
2021-01-28 14:42:11 +0300

    сканирование поля в color_sensor_print_data(туды скорее всего просто переедет вся логика и это будет главной прогой)

--- test_motors/test_motors.ino
+++ test_motors/test_motors.ino
@@ -3,32 +3,54 @@
 # define mini_servo_2 12
 # define mini_servo_3 11
 # define servo_1 11
+# define servo_0_pin 3
+
+#define BUTTON_PIN  10
 
 Servo servo1;
 Servo servo2;
 Servo servo3;
+Servo servo_0;
 
 void setup() {
   servo1.attach(mini_servo_1);
   servo2.attach(mini_servo_2);
   servo3.attach(mini_servo_3);
 
+servo_0.attach(servo_0_pin);
+servo_0.write(88);
 
 }
 
 void loop() {
-  servo1.write(0);
-  delay(1000);
-  servo2.write(0);
-  delay(1000);
-  servo3.write(0);
-  delay(1000);
-  servo1.write(180);
-  delay(1000);
-  servo2.write(180);
-  delay(1000);
-  servo3.write(180);
-  delay(1000);
+  int buttonState = digitalRead(BUTTON_PIN);
+  while (buttonState){
+    buttonState = digitalRead(BUTTON_PIN);
+    delay (100);
+  }while (not buttonState){
+    buttonState = digitalRead(BUTTON_PIN);
+  }
+  servo_0.write(180);
+  delay (900);
+  servo_0.write(88);
+  delay (900);
+  servo_0.write(180);
+  delay (810);
+  servo_0.write(88);
+  delay (900);
+  servo_0.write(180);
+  delay (810);
+  servo_0.write(88);
+  delay (900);
+
+  servo_0.write(180);
+  delay (900);
+  servo_0.write(88);
+  delay (500);
+  
+  servo_0.write(0);
+  delay (3300);
+  servo_0.write(88);
 
 
 }
